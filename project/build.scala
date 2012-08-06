52262b8d71624912d67c9cf39e4f39f052d62229
Takeshita Yoshiteru <takezoux3@Takeshita-Yoshiteru-no-MacBook-Pro.local>
2012-08-06 16:35:33 +0900

    make subproject

---
+++ project/build.scala
@@ -0,0 +1,25 @@
+import sbt._
+import Keys._
+import sbtrelease.ReleasePlugin._
+
+
+object HelloBuild extends Build {
+
+
+  lazy val root = Project(id = "hello",
+    base = file("."),
+    settings = Project.defaultSettings ++ releaseSettings ++ Seq(
+      organization := "com.geishatokyo",
+      name := "root-project"  
+    )
+  )
+
+  lazy val subProject = Project(id = "subproject",
+    base = file("subp"),
+    settings = Project.defaultSettings ++ releaseSettings ++  Seq(
+      organization := "com.geishatokyo",
+      name := "sub-project"
+    )
+  )
+
+}
---
+++ subp/App.scala
@@ -0,0 +1,9 @@
+
+
+object App{
+
+  def main(args : Array[String]) {
+    println("Sub project")
+  }
+
+}

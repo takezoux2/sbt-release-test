ebe1a575d134b1779c7201ed016756f393de1646
takeshita <takeshita@geishatokyo.com>
2012-08-06 22:48:03 +0900

    update release process

--- project/build.scala
+++ project/build.scala
@@ -2,10 +2,12 @@ import sbt._
 import Keys._
 import sbtrelease.ReleasePlugin._
 import sbtrelease.ReleasePlugin.ReleaseKeys._
+import sbtrelease.ReleaseStateTransformations._
 
 
 object HelloBuild extends Build {
-
+  
+   val mainScalaVersion = "2.9.1"
 
   lazy val root = Project(id = "hello",
     base = file("."),
@@ -15,7 +17,31 @@ object HelloBuild extends Build {
       name := "root-project",
       executableName := Some(s => "git.cmd"),
       //publishTo :=  Some("local" at "file://Users/takezoux3/program/scala/temp")
-      publishTo := Some(Resolver.file("localMaven",Path.userHome / ".m2" / "repository"))
+      publishTo := Some(Resolver.file("localMaven",Path.userHome / ".m2" / "repository")),
+      releaseProcess <<= (scalaVersion)( v => {
+        if(v == mainScalaVersion){
+          Seq(
+  checkSnapshotDependencies,              // : ReleaseStep
+  inquireVersions,                        // : ReleaseStep
+  runTest,                                // : ReleaseStep
+  setReleaseVersion,                      // : ReleaseStep
+  commitReleaseVersion,                   // : ReleaseStep, performs the initial git checks
+  tagRelease,                             // : ReleaseStep
+  publishArtifacts,                       // : ReleaseStep, checks whether `publishTo` is properly set up
+  setNextVersion,                         // : ReleaseStep
+  commitNextVersion,                      // : ReleaseStep
+  pushChanges               
+  )              
+        }else{
+          Seq(
+  checkSnapshotDependencies,              // : ReleaseStep
+  inquireVersions,                        // : ReleaseStep
+  runTest,                                // : ReleaseStep
+  setReleaseVersion,                      // : ReleaseStep
+  publishArtifacts                       // : ReleaseStep, checks whether `publishTo` is properly set up
+  )
+        }
+      })
   
     ) 
   ) aggregate(subProject)
@@ -28,7 +54,32 @@ object HelloBuild extends Build {
       name := "sub-project",
       executableName := Some(s => "git.cmd"),
       //publishTo := Some("local" at "file://Users/takezoux3/program/scala/temp")
-      publishTo := Some(Resolver.file("localMaven",Path.userHome / ".m2" / "repository"))
+      publishTo := Some(Resolver.file("localMaven",Path.userHome / ".m2" / "repository")),
+      releaseProcess <<= (scalaVersion)( v => {
+        if(v == mainScalaVersion){
+          Seq(
+  checkSnapshotDependencies,              // : ReleaseStep
+  inquireVersions,                        // : ReleaseStep
+  runTest,                                // : ReleaseStep
+  setReleaseVersion,                      // : ReleaseStep
+  commitReleaseVersion,                   // : ReleaseStep, performs the initial git checks
+  tagRelease,                             // : ReleaseStep
+  publishArtifacts,                       // : ReleaseStep, checks whether `publishTo` is properly set up
+  setNextVersion,                         // : ReleaseStep
+  commitNextVersion,                      // : ReleaseStep
+  pushChanges               
+  )              
+        }else{
+          Seq(
+  checkSnapshotDependencies,              // : ReleaseStep
+  inquireVersions,                        // : ReleaseStep
+  runTest,                                // : ReleaseStep
+  setReleaseVersion,                      // : ReleaseStep
+  publishArtifacts                       // : ReleaseStep, checks whether `publishTo` is properly set up
+  )
+        }
+      })
+  
     )
   )
 
